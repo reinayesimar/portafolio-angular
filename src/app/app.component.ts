60bcca9a11fc04e875976016c06b861b34dd1256
reinayesimar <60051323+reinayesimar@users.noreply.github.com>
2020-02-10 22:14:00 -0300

    Fin de la seccion 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { InfoPaginasService } from './services/info-paginas.service';
+import { ProductosService } from './services/productos.service';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,8 @@ import { InfoPaginasService } from './services/info-paginas.service';
 export class AppComponent {
   title = 'portafolio1';
 
-  constructor( public infoPaginaService: InfoPaginasService) {
+  constructor( public infoPaginaService: InfoPaginasService,
+               public infoProductosService: ProductosService ) {
 
   }
 }
--- src/app/services/info-paginas.service.ts
+++ src/app/services/info-paginas.service.ts
@@ -7,18 +7,31 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
   providedIn: 'root'
 })
 export class InfoPaginasService {
-  
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] =  [];
 
   constructor( private http: HttpClient ) {
-    // leer los datos del json y utilizar sus propiedades
-    this.http.get('assets/data/data-pagina.json')
-        .subscribe( (resp: InfoPagina) => {
-          this.cargada = true;
-          this.info = resp;
-          console.log(resp);
-        });
+      this.cargarInfo();
+      this.cargarEquipo();
+  }
+
+  private cargarInfo() {
+     // leer los datos del json y utilizar sus propiedades
+     this.http.get('assets/data/data-pagina.json')
+     .subscribe( (resp: InfoPagina) => {
+       this.cargada = true;
+       this.info = resp;
+     });
+  }
+
+  private cargarEquipo() {
+      // leer los datos del json y utilizar sus propiedades del firebase
+      this.http.get('https://angular-html-d4552.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[]) => {
+        this.equipo = resp;
+        
+      });
   }
 
 }
